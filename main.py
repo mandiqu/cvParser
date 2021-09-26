a397332e990c80d59efad2248150eec0cba7e468
Mandi Qu <mandiai@sina.com>
2021-09-25 19:09:03 -0600

    Improved header processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-list_id = "126890"
+REQUIRED_FIELDS = ['DESCRIPTION']
+LIST_ID = "126890"
 
 # Read in input data
 input_file = open("data/Super Powers Descriptor List.txt", 'r')
@@ -6,28 +7,46 @@ input_file = open("data/Super Powers Descriptor List.txt", 'r')
 output_file = open("data/cvCommands.txt", 'w')
 
 # Unlock the list
-output_file.write("update_list\t" + list_id + "\tlock=false\n")
+output_file.write("update_list\t" + LIST_ID + "\tlock=false\n")
 
-# Skip the header line
-skipped_headers = False
+# Process the first line as a list of headers
+headers = input_file.readline().strip().split("\t")
 
 # Loop through lines in the input file
 for line in input_file:
-    if not skipped_headers:
-        skipped_headers = True
+    # For each header, place the field into a dictionary
+    current_concept = {}
+    line_parts = line.replace("\n", "").split("\t")
+    for index in range(0, len(headers)):
+        current_concept[headers[index]] = line_parts[index]
+
+    # Validate the concept - do not output if it doesn't contain required fields
+    failed_validation = False
+    for field in REQUIRED_FIELDS:
+        if field not in current_concept or current_concept[field].strip() == "":
+            print("Could not find required field " + field + " in concept!")
+            failed_validation = True
+            break
+    if failed_validation:
         continue
-    line_parts = line.strip().split("\t")
+
     # Create the concept
-    output_file.write("create_concept\t" + line_parts[1] + "\n")
-    # Set the concept type
-    output_file.write("update_concept\t$conceptId\t" + "attrs=1:" + line_parts[0] + "\n")
-    # Create the official term
-    output_file.write("create_term\t" + line_parts[2] + "\ten\tOFFICIAL\t$conceptId\n")
-    output_file.write("update_list\t" + list_id + "\tterms=$termId\n")
+    output_file.write("create_concept\t" + current_concept['DESCRIPTION'] + "\n")
+    # Set the concept type if it exists
+    if 'TYPE' in current_concept and current_concept['TYPE'].strip() != "":
+        output_file.write("update_concept\t$conceptId\t" + "attrs=1:" + current_concept['TYPE'] + "\n")
+    # If the official term exists, create it
+    if 'OFFICIAL' in current_concept and current_concept['OFFICIAL'].strip() != "":
+        output_file.write("create_term\t" + current_concept['OFFICIAL'] + "\ten\tOFFICIAL\t$conceptId\n")
+        output_file.write("update_list\t" + LIST_ID + "\tterms=$termId\n")
+    # If an official plural term exists, create it
+    if 'OFFICIAL_PLURAL' in current_concept and current_concept['OFFICIAL_PLURAL'].strip() != "":
+        output_file.write("create_term\t" + current_concept['OFFICIAL_PLURAL'] + "\ten\tOFFICIAL_PLURAL\t$conceptId\n")
+        output_file.write("update_list\t" + LIST_ID + "\tterms=$termId\n")
     # If an alternate term exists, create it
-    if len(line_parts) > 3:
-        output_file.write("create_term\t" + line_parts[3] + "\ten\tALTERNATE\t$conceptId\n")
-        output_file.write("update_list\t" + list_id + "\tterms=$termId\n")
+    if 'ALTERNATE' in current_concept and current_concept['ALTERNATE'].strip() != "":
+        output_file.write("create_term\t" + current_concept['ALTERNATE'] + "\ten\tALTERNATE\t$conceptId\n")
+        output_file.write("update_list\t" + LIST_ID + "\tterms=$termId\n")
 
 # Lock the list
-output_file.write("update_list\t" + list_id + "\tlock=true\n")
+output_file.write("update_list\t" + LIST_ID + "\tlock=true\n")
